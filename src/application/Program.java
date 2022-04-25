2412a57d52d2f4b4714ec5809930a90e873ee97d
pedroplb <pedroplb@gmail.com>
2022-04-25 20:43:00 -0300

    primeiro commit

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+import jogoTabuleiro.Tabuleiro;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+
+	}
+
+}
---
+++ src/jogoTabuleiro/Tabuleiro.java
@@ -0,0 +1,37 @@
+package jogoTabuleiro;
+
+public class Tabuleiro {
+	
+	private int linha;
+	private int coluna;
+	/* matriz de peças*/
+	private Peca[][] pecas;
+	
+	public Tabuleiro(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+		pecas = new Peca[linha][coluna];
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public void setLinha(int linha) {
+		this.linha = linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+
+	public void setColuna(int coluna) {
+		this.coluna = coluna;
+	}
+	
+	
+	
+	
+	
+
+}
