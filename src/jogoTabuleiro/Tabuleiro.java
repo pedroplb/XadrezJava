568af277268607e6589edc3577e45a3dffb88da4
pedroplb <pedroplb@gmail.com>
2022-04-27 20:50:45 -0300

    Tratando exceção de peça e posição

--- src/jogoTabuleiro/Tabuleiro.java
+++ src/jogoTabuleiro/Tabuleiro.java
@@ -8,6 +8,11 @@ public class Tabuleiro {
 	private Peca[][] pecas;
 	
 	public Tabuleiro(int linha, int coluna) {
+		
+		if(linha <1 || coluna  <1) {
+			throw new TabuleiroException("Quantidade de linhas e colunas inválidas");
+		}
+		
 		this.linha = linha;
 		this.coluna = coluna;
 		pecas = new Peca[linha][coluna];
@@ -17,30 +22,46 @@ public class Tabuleiro {
 		return linha;
 	}
 
-	public void setLinha(int linha) {
-		this.linha = linha;
-	}
-
 	public int getColuna() {
 		return coluna;
 	}
-
-	public void setColuna(int coluna) {
-		this.coluna = coluna;
-	}
 	
 	public Peca peca(int linha, int coluna) {
+		if(!posicaoExiste(linha,coluna)) {
+			throw new TabuleiroException("Posição inválida");
+		}
 		return pecas[linha][coluna];
 	}
 	
 	public Peca peca(Posicao posicao) {
+		if(!posicaoExiste(posicao)) {
+			throw new TabuleiroException("Posição inválida");
+		}
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
 	
 	public void colocaPeca(Peca peca, Posicao posicao) {
+		if(ehPeca(posicao)) {
+			throw new TabuleiroException("Posição Ocupada " + posicao);
+		}
+		
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
 	}
 	
+	private boolean posicaoExiste (int linha, int coluna) {
+		//dependendo da condição retorna verdadeiro ou falso
+		return (linha>=0 && coluna>=0 && linha<this.linha && coluna<this.coluna);
+	}
 	
+	public boolean posicaoExiste(Posicao posicao) {
+		return posicaoExiste(posicao.getLinha(), posicao.getColuna());
+	}
+	
+	public boolean ehPeca(Posicao posicao) {
+		if(!posicaoExiste(posicao)) {
+			throw new TabuleiroException("Posição inválida");
+		}
+		return peca(posicao)!=null;
+	}
 }
---
+++ src/jogoTabuleiro/TabuleiroException.java
@@ -0,0 +1,11 @@
+package jogoTabuleiro;
+
+public class TabuleiroException extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public TabuleiroException (String msg) {
+		super(msg);
+	}
+
+}
