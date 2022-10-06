3e8f15721372841b75a5914ed6bcbc4b25f09adc
pedroplb <pedroplb@gmail.com>
2022-10-06 18:56:34 -0300

    contador de movimento

--- src/Xadrez/PecaXadrez.java
+++ src/Xadrez/PecaXadrez.java
@@ -7,6 +7,7 @@ import jogoTabuleiro.Tabuleiro;
 public abstract class PecaXadrez extends Peca{
 	
 	private Cor cor;
+	private int contaMovimento;
 
 	public PecaXadrez(Tabuleiro tabuleiro, Cor cor) {
 		super(tabuleiro);
@@ -15,6 +16,18 @@ public abstract class PecaXadrez extends Peca{
 
 	public Cor getCor() {
 		return cor;
+	}
+	
+	public int getContaMovimento() {
+		return contaMovimento;
+	}
+	
+	public void somaContaMovimento() {
+		contaMovimento++;
+	}
+	
+	public void subtraiContaMovimento() {
+		contaMovimento--;
 	}
 	
 	public PosicaoXadrez getPosicaoXadrez() {
